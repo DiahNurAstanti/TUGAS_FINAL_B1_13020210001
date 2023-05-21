f6722ac1337b7ab3d273d2d7b6df8d25a01595a4
DIAH NUR ASTANTI <127530115+DiahNurAstanti@users.noreply.github.com>
2023-05-22 02:00:00 +0800

    Create Penjualan_rumah.java

---
+++ Penjualan_rumah.java
@@ -0,0 +1,16 @@
+
+package penjualan_rumah;
+
+import penjualan_rumah.view.ViewPenjualanRumah;
+
+
+public class Penjualan_rumah {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new ViewPenjualanRumah().setVisible(true);
+    }
+    
+}
